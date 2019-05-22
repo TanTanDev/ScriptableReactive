0bffd2c2d03d2784d8bfad11869f705256645e1f
TanTanDev <mrjonatanOlsson@gmail.com>
2019-05-22 14:48:54 +0200

    Add MonoUpdaterVector3Position

---
+++ Code/MonoUpdater/MonoUpdaterVector3Position.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MonoUpdaterVector3Position : MonoBehaviour
+{
+    [SerializeField] private ReactiveVector3 m_reactiveVector3;
+
+    void Update()
+    {
+        if(m_reactiveVector3 != null)
+            m_reactiveVector3.OnNext(transform.position);
+    }
+}
